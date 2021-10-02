c7b33cdb9ce0e84ee0bfc93b6eea2de2dbbceb40
Shariq Hirani <shariqhirani2010@gmail.com>
2021-10-02 12:15:20 -0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 
+import {ThemeProvider} from '../lib/ThemeContext'
+
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+    )
 }
 
 export default MyApp
